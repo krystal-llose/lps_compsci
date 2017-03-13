bc278ba7a0d4722465a1573f7003c93bab2295fe
Krystal Llose <rc.llose.krystal@leadps.org>
2017-03-13 03:57:33 +0000

    uncompleted team manager deluxe

---
+++ problem_sets/ps7/teamManagerDeluxe.py
@@ -0,0 +1,19 @@
+# teamManagerDeluxe.py
+
+class Player(object):
+
+	def __init__(self, name, age, goals, jersey, position):
+		self.name = name
+    		self.age = age
+    		self.goals = goals
+    		self.number = number
+    		self.position = position
+
+ 	def getStats(self):
+                print("Player: " + self.name)
+                print("Age: " + str(self.age))
+                print("Goals: " + str(self.goals))
+		print("Number: " + str(self.number))
+		print("Position: " + self.position)
+		
+ 
