bc977da9775d8ae596fa0a3e20c138e5dc50ce25
Krystal Llose <rc.llose.krystal@leadps.org>
2017-03-23 15:57:59 +0000

    Added existing samples.

---
+++ class_samples/4-8_variablescope/addBruh.py
@@ -0,0 +1,18 @@
+def addBruh(myWords):
+	modifiedWords = "Bruh! " + myWords
+	
+	# avoid doing this, even though it works 
+	print(sentence)
+
+	return modifiedWords
+
+# test the function
+sentence = "I want a cat!"
+newSentence = addBruh(sentence)
+
+print("The old sentence: " + sentence)
+print("The new sentence: " + newSentence)
+
+# this should not work
+print(myWords)
+
