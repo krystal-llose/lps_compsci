f35f9df1babf5805d3da24fb90b8714d5ffbac93
Krystal Llose <rc.llose.krystal@leadps.org>
2017-03-23 16:05:36 +0000

    Added existing samples.

---
+++ class_samples/5-5_dictionaries/launch.py
@@ -0,0 +1,11 @@
+# launch.py 21 March
+
+myGrades = {
+	'Suleima': 100,
+	'Anita': 50,
+	'Jenny': 99,
+	'Carlos': 12 
+}
+
+for person in myGrades:
+	print(person + ' has a ' + str(myGrades[person]))
